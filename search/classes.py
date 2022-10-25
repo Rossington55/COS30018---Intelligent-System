097293c75dc39c3f91f0ce68f14be13817ef4c34
Jackster111 <84361338+Jackster111@users.noreply.github.com>
2022-10-25 13:27:34 +1100

    First instance of route navigation
    A Star and relevant classes

---
+++ search/classes.py
@@ -0,0 +1,57 @@
+
+class Node:
+    def __init__(self, X, Y):
+        self._x = X
+        self._y = Y
+        
+    #temp coordinate system to represent lon/lat
+    def get_x(self):
+        return self._x
+    def get_y(self):
+        return self._y
+    #generate loc from given coordinates in constructor
+    def set_loc(self, value):
+        self._loc = value
+    def get_loc(self):
+        return self._loc
+    
+    #properties used by search algo
+    def set_parentNode(self, value):
+        self._parentNode = value
+    def get_parentNode(self):
+        return self._parentNode
+    
+    def set_tDist(self, value):
+        self._tDist = value
+    def get_tDist(self):
+        return self._tDist
+    
+    def set_distToGoal(self, value):
+        self._distToGoal = value    
+    def get_distToGoal(self):
+        return self._distToGoal
+    
+    def set_distToStart(self, value):
+        self._distToStart = value  
+    def get_distToStart(self):
+        return self._distToStart
+    
+    
+    #collection of all interconnecting streets as nodes
+class Map:
+    def __init__(self, start, end, nodes):
+        self._nodes = nodes
+        self._start = start
+        self._end = end
+    
+    def get_start(self):
+        return self._start
+    
+    def get_end(self):
+        return self._end    
+    
+    def get_node(self, node):
+        #check if node location matches a node in _nodes
+        for curNode in self._nodes:
+            if curNode.get_loc() == node.get_loc():
+                return curNode
---
+++ search/search.py
@@ -0,0 +1,54 @@
+import classes
+
+def distToGoal(node):
+    return node.distToGoal
+
+
+def aStarSearch(map):
+    start = map.get_start()
+    end = map.get_end()
+    searchedNodes = []
+    goalReached = False
+    
+    openList = [] 
+    openList.append(start)
+    
+    while openList.count > 0:
+        curNode = openList.pop()
+        
+        for i in len(openList):
+            if curNode.tDist > openList[i].tDist:
+                curNode = openList[i]          
+        
+        searchedNodes.append(curNode)
+        
+        if curNode == end:
+            goalReached = True
+            break
+        
+        childNodes = []
+        #Check each street/scatsSite from current node
+        for adjNode in curNode.adjNodes:
+            workingNode = map.getNode(adjNode)
+            workingNode.parentNode = curNode
+            childNodes.append(workingNode)
+    
+        childNodes.sort(key=distToGoal)
+        
+        for childNode in childNodes:
+            skip = False
+            childNode.distToStart = curNode.distToStart #(+ ?) #add distance between intersections/scats sites
+            childNode.tDist = childNode.distToStart + childNode.distToGoal
+            childNode.parentNode = curNode
+            
+            for cell in openList:
+                if childNode.X == cell.X and childNode.Y == cell.Y:
+                    if childNode.tDist >= map.getNode(cell).tDist:
+                        skip = True
+                
+        if skip:
+            openList.append(childNode)
+        
+    if goalReached:
+        return searchedNodes
+    return

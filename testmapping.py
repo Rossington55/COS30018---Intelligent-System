7926231b1c896a539c7df2aa903e276ca760ce9c
s103099247 <103099247@student.swin.edu.au>
2022-10-29 16:40:17 +1100

    Created Testing file for connecting map

---
+++ testmapping.py
@@ -0,0 +1,34 @@
+"""
+Traffic Flow Prediction with Neural Networks(SAEs、LSTM、GRU).
+"""
+import math
+import warnings
+import numpy as np
+import pandas as pd
+from data.data import process_map, get_scats_list
+from keras.models import load_model
+from keras.utils.vis_utils import plot_model
+import sklearn.metrics as metrics
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def main():
+
+    file1 = 'data/data1.xls'
+
+    for scat in get_scats_list(file1):
+        node = process_map(file1, scat[0])
+        print('SCAT : ' + str(node[0]))
+        print('Longitude : ' + str(node[1]))
+        print('Latitude : ' + str(node[2]))
+        print('Street Connections : ')
+        for connection in node[3]:
+            print('Street Name: ' + connection[0])
+            print('Direction: ' + connection[1])
+            print('Connected SCAT: ' + str(connection[2]))
+
+        
+
+
+if __name__ == '__main__':
+    main()
